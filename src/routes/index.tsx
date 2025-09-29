f10dad2490c38d9044f9818621a50c9683000965
Josh Strunk <joshstrunk226@gmail.com>
2025-09-29 16:20:28 -0400

    Adding league creation

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,7 +22,6 @@ export const route = {
         search: { name: league.name, status: league.status, id: league.leagueid }
       });
     }
-
     
     const [leagues, setLeagues] = useState<LeagueInformation[]>([]);
     useEffect(() => {
@@ -42,13 +41,47 @@ export const route = {
       }
     }, [loggedInUser]);
 
+    const [isOpen, setIsOpen] = useState(false);
+    const [leagueName, setLeagueName] = useState("");
+
+    const createLeague = () => {
+      console.log('creating league', leagueName);
+      fetch(`http://localhost:3000/createleague?leaguename=${encodeURIComponent(leagueName)}`)
+      .then(res => {
+        if (!res.ok) throw new Error('Network response was not ok');
+        return res.json();
+      })
+      .then(data => {
+        console.log(data);
+        let newleagueid = data.id;
+
+        fetch(`http://localhost:3000/leagueadduser?leagueid=${encodeURIComponent(newleagueid)}&teamname=${encodeURIComponent(loggedInUser ?? '')}`)
+        .then(res => {
+          if (!res.ok) throw new Error('Network response was not ok');
+          return res.json();
+        })
+        .then(data => {
+          console.log(data);
+          navigate({
+            to: '/league',
+            replace: true, 
+            search: { name: leagueName, status: 'Pre-Draft', id: newleagueid }
+          });
+        })
+        .catch(err => console.log(err.message));
+      })
+      .catch(err => console.log(err.message));
+    };
 
     return (
       <div>
         <h1>Playoff Push</h1>
         <h1>Welcome, {loggedInUser}!</h1>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: "space-between", margin: '15px 2px 0 2px'}}>
+          <h1>Your Leagues</h1>
+          <h1 onClick={() => setIsOpen(true)} style={{ cursor: "pointer" }}>Create League</h1>
+        </div>
         
-        <h1 style={{marginTop: '15px'}}>Your Leagues</h1>
         <div className='league-list'>
           {leagues.map((league) => (
             <div className='league' onClick={() => handleClick(league)}> 
@@ -57,6 +90,19 @@ export const route = {
             </div>
           ))}
         </div>
+
+        {isOpen && (
+        <>
+          <div className="modal-backdrop" onClick={() => setIsOpen(false)} />
+          <div className="modal">
+            <h2>Create a New League</h2>
+            <input type="text" value={leagueName}
+              onChange={(e) => setLeagueName(e.target.value)}
+              placeholder="League Name" autoFocus />
+            <button onClick={() => createLeague()}>Create League</button>
+          </div>
+        </>
+      )}
       </div>
     );
   },
